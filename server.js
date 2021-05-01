58d8e0fb22643c78e201a563b4f8f2413051885b
Samantha <Sambollano54@gmail.com>
2021-05-01 11:16:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
